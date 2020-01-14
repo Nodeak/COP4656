56c75267c5f1770c9514da62c57701ad9d0103d6
Nodeak <kaedon.hamm@gmail.com>
2020-01-14 18:57:06 -0500

    Add button to change 'Hello World' to 'Kaedon Hamm'

--- HW1_Hamm_Kaedon/app/src/main/java/com/fsu/cs/hw1_hamm_kaedon/MainActivity.java
+++ HW1_Hamm_Kaedon/app/src/main/java/com/fsu/cs/hw1_hamm_kaedon/MainActivity.java
@@ -1,14 +1,26 @@
-        package com.fsu.cs.hw1_hamm_kaedon;
+package com.fsu.cs.hw1_hamm_kaedon;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private Button mButton;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.button);
+        mTextView = (TextView) findViewById(R.id.textView);
+    }
+
+
+    public void replaceText(View view){
+        mTextView.setText("Kaedon Hamm");
     }
 }
